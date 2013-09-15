526833d3093205650d821600ad79a8064f226fa1
舛田 明寛 <makihiro_dn8@goo.jp>
2013-09-15 17:44:07 +0900

    カメラ撮影するサンプルを追加

---
+++ test06_1_camera/test06_1_camera/MyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyAppDelegate.h
+//  test06_1_camera
+//
+//  Created by 舛田 明寛 on 2013/09/15.
+//  Copyright (c) 2013年 AkihiroMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test06_1_camera/test06_1_camera/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test06_1_camera
+//
+//  Created by 舛田 明寛 on 2013/09/15.
+//  Copyright (c) 2013年 AkihiroMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
+    }
+}
