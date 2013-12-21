42e2d69b0045e5af50151e8ee0debd6d5356b290
舛田 明寛 <makihiro_dn8@goo.jp>
2013-12-21 14:24:18 +0900

    ボタン２つのケースに修正

--- test09_2_AutoLaytout/test09_AutoLaytout/MainViewController.m
+++ test09_2_AutoLaytout/test09_AutoLaytout/MainViewController.m
@@ -54,23 +54,34 @@
     [viewBase setTranslatesAutoresizingMaskIntoConstraints:NO]; // 必須
     
     // ボタン生成
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    [btn setTitle:@"ボタン" forState:UIControlStateNormal];
-    btn.backgroundColor = [UIColor greenColor];
+    UIButton *btn1 = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn1 setTitle:@"ボタン1" forState:UIControlStateNormal];
+    btn1.backgroundColor = [UIColor greenColor];
+    UIButton *btn2 = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn2 setTitle:@"ボタン2" forState:UIControlStateNormal];
+    btn2.backgroundColor = [UIColor greenColor];
     // 基礎ビューに追加
-    [viewBase addSubview:btn]; //制約設定前にこれ追加してないとダメっぽい
+    [viewBase addSubview:btn1]; //制約設定前にこれ追加してないとダメっぽい
+    [viewBase addSubview:btn2]; //制約設定前にこれ追加してないとダメっぽい
     // AutoLayout有効にするためのおまじない
-    [btn setTranslatesAutoresizingMaskIntoConstraints:NO]; // 必須
+    [btn1 setTranslatesAutoresizingMaskIntoConstraints:NO]; // 必須
+    [btn2 setTranslatesAutoresizingMaskIntoConstraints:NO]; // 必須
     
     // AutoLayoutの設定(基礎ビュー上のボタンについて)
     {
-        NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(btn); //ここで指定した変数名が、下の設定で使われる
-        NSArray *constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(8)-[btn]-(60)-|" //親ビューから左8,右60間を開け、ボタンの幅はそれに合わせる
+        NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(btn1,btn2); //ここで指定した変数名が、下の設定で使われる
+        NSArray *constraints;
+        constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(8)-[btn1]-(60)-|" //親ビューから左8,右60間を開け、ボタン1の幅はそれに合わせる
                                                                        options:0
                                                                        metrics:nil
                                                                          views:viewsDictionary];
         [viewBase addConstraints:constraints];
-        constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(16)-[btn(100)]|" //親ビューから上16の間を開け、ボタン高さを100とする
+        constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(8)-[btn2]-(60)-|" //親ビューから左8,右60間を開け、ボタン2の幅はそれに合わせる
+                                                              options:0
+                                                              metrics:nil
+                                                                views:viewsDictionary];
+        [viewBase addConstraints:constraints];
+        constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(16)-[btn1(100)]-(16)-[btn2(100)]" //親ビューから上16の間を開け、ボタン1高さを100で配置。その下16開けてボタン2を高さ100で配置（最後に|との接続を書かない点注意）
                                                               options:0
                                                               metrics:nil
                                                                 views:viewsDictionary];
