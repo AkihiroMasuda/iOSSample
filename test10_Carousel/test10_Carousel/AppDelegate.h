89b7c48b9f6d1375afade1d8d9ae921b05637266
舛田 明寛 <makihiro_dn8@goo.jp>
2014-02-23 10:11:00 +0900

    Initial Commit

---
+++ test10_Carousel/test10_Carousel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test10_Carousel
+//
+//  Created by 舛田 明寛 on 2014/02/23.
+//  Copyright (c) 2014年 AkihiroMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
