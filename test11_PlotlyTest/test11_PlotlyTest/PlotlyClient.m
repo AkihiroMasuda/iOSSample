111439a67bb28006a991882c79f7be79038ddabe
舛田 明寛 <makihiro_dn8@goo.jp>
2014-03-08 17:50:49 +0900

    plotly操作部分を分離

---
+++ test11_PlotlyTest/test11_PlotlyTest/PlotlyClient.m
@@ -0,0 +1,38 @@
+//
+//  PlotlyClient.m
+//  test11_PlotlyTest
+//
+//  Created by 舛田 明寛 on 2014/03/08.
+//  Copyright (c) 2014年 AkihiroMasuda. All rights reserved.
+//
+
+#import "PlotlyClient.h"
+
+@interface PlotlyClient ()
+
+@end
+
+@implementation PlotlyClient
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
